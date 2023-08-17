6d601d5d33a1f341810e0fb635115f4cfe7b3352
Mr.Rolex <139060034+yogi444@users.noreply.github.com>
2023-08-17 17:18:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -64,6 +64,7 @@ async def start(client, message):
             parse_mode=enums.ParseMode.HTML
         )
         return
+        +
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
             invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
@@ -81,8 +82,7 @@ async def start(client, message):
         if message.command[1] != "subscribe":
             try:
                 kk, file_id = message.command[1].split("_", 1)
-                pre = 'checksubp' if kk == 'filep' else 'checksub' 
-                btn.append([InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", callback_data=f"{pre}#{file_id}")])
+                btn.append([InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", callback_data=f"checksub#{kk}#{file_id}")])
             except (IndexError, ValueError):
                 btn.append([InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_message(
