9f802f449dc49f5151ef4bfe43505374de2b7ce1
Mr.Rolex <139060034+yogi444@users.noreply.github.com>
2023-09-23 05:02:48 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -198,10 +198,10 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-        CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name} 
-  
- <b> Size ⚙️: {file_size}</b>""" 
+    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
 
+<b>Size ⚙️: {file_size}</b>""" 
+    
     IMDB_TEMPLATE_TXT = """
 <b>Query: {query}
 IMDb Data:
